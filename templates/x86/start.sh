009ecc4863ab2ce96de45eb797914b2a2d43ac29
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2018-11-28 15:24:12 +0100

    Adding a x86 template to build a vanilla i686 vm

---
+++ templates/x86/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Create mount point if needed
+mkdir -p mountpoint/
+
+# Running QEMU
+qemu-system-x86_64 \
+    -kernel ./bzImage \
+    -initrd ./initramfs.cpio.gz  \
+    -append 'console=ttyS0 loglevel=3 oops=panic panic=1' \
+    -fsdev local,id=exp1,path=./mountpoint,security_model=mapped \
+    -device virtio-9p-pci,fsdev=exp1,mount_tag=mountpoint \
+    -no-reboot \
+    -nographic
